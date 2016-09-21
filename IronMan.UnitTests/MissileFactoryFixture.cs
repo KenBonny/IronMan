1c3f8bce5d1083b5fb3ce61debc3de85f1ceb9e6
Ken Bonny <bonny.ken+github@gmail.com>
2016-09-21 22:21:14 +0200

    Add MissileFactoryFixture.

---
+++ IronMan.UnitTests/MissileFactoryFixture.cs
@@ -0,0 +1,14 @@
+﻿using IronMan.Weapons;
+
+namespace IronMan.UnitTests
+{
+    public class MissileFactoryFixture
+    {
+        public MissileFactory Factory { get; set; }
+
+        public MissileFactoryFixture()
+        {
+            Factory= new MissileFactory();
+        }
+    }
+}

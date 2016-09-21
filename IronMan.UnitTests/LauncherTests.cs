9cdbfe1a3e405a2109163bd8295109236696960e
Ken Bonny <bonny.ken+github@gmail.com>
2016-09-21 21:55:21 +0200

    prepare data driven test example.

--- IronMan.UnitTests/LauncherTests.cs
+++ IronMan.UnitTests/LauncherTests.cs
@@ -25,43 +25,5 @@ namespace IronMan.UnitTests
             Assert.Equal(0, launcher.MissileCount);
             Assert.True(launcher.MissileCount == 0, "Missile was not launched");
         }
-
-        public static IEnumerable<object[]> MissileData
-        {
-            get
-            {
-                return new[]
-                {
-                    new object[] {new HeatSeekingMissile()},
-                    new object[] {new LaserGuidedMissile()}
-                };
-            }
-        }
-
-        [Theory]
-        [MemberData(nameof(MissileData))]
-        [Trait("Category", "ComponentTest")]
-        public void When_launching_missile_from_memberdata_then_decrease_missile_count_by_1(IMissile missile)
-        {
-            // arrange
-            var launcher = new Launcher(new List<IMissile> {missile});
-            // act
-            launcher.Launch();
-            // assert
-            Assert.Equal(0, launcher.MissileCount);
-        }
-
-        [Theory]
-        [ClassData(typeof(MissileClassData))]
-        [Trait("Category", "ComponentTest")]
-        public void When_launching_missile_from_classdata_then_decrease_missile_count_by_1(IMissile missile)
-        {
-            // arrange
-            var launcher = new Launcher(new List<IMissile> {missile});
-            // act
-            launcher.Launch();
-            // assert
-            Assert.Equal(0, launcher.MissileCount);
-        }
     }
 }
--- IronMan.UnitTests/MissileFactoryTests.cs
+++ IronMan.UnitTests/MissileFactoryTests.cs
@@ -8,40 +8,27 @@ namespace IronMan.UnitTests
 {
     public class MissileFactoryTests
     {
-        [Theory]
-        [InlineData(MissileType.Guided, typeof(LaserGuidedMissile))]
-        [InlineData(MissileType.Autonomous, typeof(HeatSeekingMissile))]
-        public void When_creating_missiletype_then_expect_type(MissileType missileType, Type type)
+        [Fact]
+        public void When_creating_guided_then_expect_LaserGuidedMissile()
         {
             // arrange
             var factory = new MissileFactory();
             // act
-            var missile = factory.Create(missileType);
+            var missile = factory.Create(MissileType.Guided);
             // assert
-            Assert.IsType(type, missile);
+            Assert.IsType<LaserGuidedMissile>(missile);
         }
 
-        //[Fact]
-        //public void When_creating_guided_then_expect_LaserGuidedMissile()
-        //{
-        //    // arrange
-        //    var factory = new MissileFactory();
-        //    // act
-        //    var missile = factory.Create(MissileType.Guided);
-        //    // assert
-        //    Assert.IsType<LaserGuidedMissile>(missile);
-        //}
-
-        //[Fact]
-        //public void When_creating_autonomous_then_expect_HeatSeekingMissile()
-        //{
-        //    // arrange
-        //    var factory = new MissileFactory();
-        //    // act
-        //    var missile = factory.Create(MissileType.Autonomous);
-        //    // assert
-        //    Assert.IsType<HeatSeekingMissile>(missile);
-        //}
+        [Fact]
+        public void When_creating_autonomous_then_expect_HeatSeekingMissile()
+        {
+            // arrange
+            var factory = new MissileFactory();
+            // act
+            var missile = factory.Create(MissileType.Autonomous);
+            // assert
+            Assert.IsType<HeatSeekingMissile>(missile);
+        }
 
         [Fact]
         public void When_creating_unkown_then_expect_exception()
